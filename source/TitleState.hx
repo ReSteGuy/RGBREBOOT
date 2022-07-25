c44c3789c69ba2a2475c43825dc8cfae77ed3f0b
RSGuy <63218795+RedstoneGuyLol@users.noreply.github.com>
2022-07-25 00:29:07 -0400

    100 Best Quotes From Coco 1. “ Nothing's more important than family.” -Miguel. “Día de Muertos has begun! ... 3. “ ... “Only a song has the power to change a heart. ... 5. “ ... “Music tore her family apart. ...

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -451,7 +451,7 @@ class TitleState extends MusicBeatState
 					if (mustUpdate) {
 						MusicBeatState.switchState(new OutdatedState());
 					} else {
-						MusicBeatState.switchState(new BeforeWarningState());
+						MusicBeatState.switchState(new StoryMenuState());
 					}
 					closedState = true;
 				});

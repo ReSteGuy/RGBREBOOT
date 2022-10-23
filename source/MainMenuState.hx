16088be534af5c059a68d6673511850898c8d1a5
RSGuy <63218795+RedstoneGuyLol@users.noreply.github.com>
2022-10-23 10:48:43 -0400

    highighioh

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -123,7 +123,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
-			menuItem.x += 90;
+			menuItem.x += 65;
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
